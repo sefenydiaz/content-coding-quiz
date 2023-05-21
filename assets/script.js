6661256a23c6fab2e7e21d1a85c2a836308cb767
silentdiamond <132628168+sefenydiaz@users.noreply.github.com>
2023-05-21 15:24:41 -0400

    added questions variable

--- assets/script.js
+++ assets/script.js
@@ -7,6 +7,48 @@
 var quizId = document.getElementById('quiz');
 var resultsId = document.getElementById('results');
 var submitButton = document.getElementById('submit');
+var jsQuestions = [
+    {
+        // COME UP WITH QUESTION 1 !!
+        question: "JS question 1",
+        answers: {
+            a: 'a',
+            b: 'b',
+            c: 'c',
+        },
+        correctAnswer: 'b'
+    },
+    {
+        // COME UP WITH QUESTION 2 !!!
+        question: "JS question 2",
+        answers: {
+            a: 'a',
+            b: 'b',
+            c: 'c',
+        },
+        correctAnswer: 'a'
+    },
+    {
+        // COME UP WITH QUESTION 3 !!!
+        question: "JS question 3",
+        answers: {
+            a: 'a',
+            b: 'b',
+            c: 'c',
+        },
+        correctAnswer: 'c'
+    },
+    {
+        // COME UP WITH QUESTION 4 !!!
+        question: "JS question 4",
+        answers: {
+            a: 'a',
+            b: 'b',
+            c: 'c',
+        },
+        correctAnswer: 'a'
+    }
+];
 
 
 
